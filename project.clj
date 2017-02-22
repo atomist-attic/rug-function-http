66a7eae48e23fee5a5d91108d92776755d9b9def
Jim Clark <slimslenderslacks@gmail.com>
2017-02-22 11:21:14 -0800

    Add Midje support to a Leiningen project

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject com.atomist.rug.functions/rug-function-http "0.0.1-SNAPSHOT"
   :description "HTTP Rug Function"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
