7f2a1461c57a08c38ea34e52b22922b81c8b21a5
James Carnegie <james@atomist.com>
2017-02-22 23:11:03 +0000

    Assumes GET, but works

--- project.clj
+++ project.clj
@@ -1,5 +1,18 @@
 (defproject com.atomist.rug.functions/rug-function-http "0.0.1-SNAPSHOT"
   :description "HTTP Rug Function"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.atomist/rug "0.13.0-SNAPSHOT"]
+                 [clj-http "3.4.1"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+                   :plugins [[lein-midje "3.1.3"]
+                             [jonase/eastwood "0.2.1"]
+                             [lein-cloverage "1.0.6"]
+                             [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]]}}
+  :aot [com.atomist.rug.functions.rug-function-http.core]
+  :javac-options     ["-target" "1.8" "-source" "1.8"]
+  :repositories [["public-atomist-release" {:url      "https://atomist.jfrog.io/atomist/libs-release"}]
+                 ["releases" {:url      "https://atomist.jfrog.io/atomist/libs-release-local"
+                              :sign-releases false
+                              :username [:gpg :env/artifactory_user]
+                              :password [:gpg :env/artifactory_pwd]}]
+                 ["atomist-public" {:url "https://atomist.jfrog.io/atomist/libs-release"}]])
--- src/com/atomist/rug/functions/rug_function_http/core.clj
+++ src/com/atomist/rug/functions/rug_function_http/core.clj
@@ -1,1 +1,71 @@
-(ns com.atomist.rug.functions.rug-function-http.core)
+(ns com.atomist.rug.functions.rug-function-http.core
+  (:gen-class
+    :name com.atomist.rug.functions.HttpRugFunction
+    :init init
+    :constructors {[] []}
+    :implements [com.atomist.rug.spi.RugFunction])
+  (:require [clj-http.client :as client])
+  (:import (com.atomist.param ParameterValues Parameter)
+           (scala.collection Seq JavaConversions)
+           (com.atomist.rug.spi Handlers$Response Handlers$ Handlers$Status$Success$)
+           (scala None Option Some)))
+
+;
+; HTTP Rug Function
+;
+
+(defn- scalarize
+  "Convert clojure collections to scala ones"
+  [list]
+  (if (sequential? list)
+    (.toList
+      (JavaConversions/asScalaBuffer list))))
+
+(defn- javarize
+  [coll]
+  (JavaConversions/mapAsJavaMap coll))
+
+(defn -init
+  []
+  [[] nil])
+
+(defn -run
+  "For now, just extract url and use GET"
+  [this ^ParameterValues params]
+  (let [results (client/get (-> params (.parameterValueMap) (javarize) (get "url") (.getValue)))]
+    (Handlers$Response. Handlers$Status$Success$/MODULE$, nil, nil, (Some. results))))
+
+(defn -name
+  [this]
+  "http")
+
+(defn- tags
+  [this]
+  (scalarize ["http" "rug" "function" "clojure"]))
+
+(defn -description
+  [this]
+  "HTTP Rug Function based on clj-http")
+
+(defn -secrets
+  "No secrets for now"
+  [this]
+  (scalarize []))
+
+(defn -parameters
+  "A single one containing the full spec"
+  [this]
+  (scalarize
+    [(->
+       (Parameter. "url")
+       (.setPattern "@url")
+       (.describedAs "The HTTP URL to call"))
+     (->
+       (Parameter. "method")
+       (.setPattern "^get|put|post|head$")
+       (.describedAs "The HTTP method to use. One of get|put|post|head"))
+     (->
+       (Parameter. "config")
+       (.setPattern "@any")
+       (.setRequired false)
+       (.describedAs "Config for the HTTP client as per: https://github.com/dakrone/clj-http"))]))
