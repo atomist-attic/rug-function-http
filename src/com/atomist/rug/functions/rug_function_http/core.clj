d980608bc47f07f12f4697e40d72b451c9425180
James Carnegie <james@atomist.com>
2017-04-13 19:30:48 +0100

    Add some validation to http method

--- src/com/atomist/rug/functions/rug_function_http/core.clj
+++ src/com/atomist/rug/functions/rug_function_http/core.clj
@@ -55,14 +55,31 @@
     val
     (first default)))
 
+(def valid-methods #{"get" "put" "post" "head" "delete"})
+
+(defn validate-method
+  "Is the method allowed?. Throw exceptions if not"
+  [method]
+  (let [lower (.toLowerCase method)]
+    (if (contains? valid-methods lower)
+      lower
+      (throw (RuntimeException. (str "Method must be one of: " (pr-str valid-methods)))))))
+
+(defn run
+  "Clean version of -run"
+  [method url config]
+  (apply
+    (resolve (symbol (str "clj-http.client/" (validate-method method))))
+    [url
+     (-> config (json/read-str :key-fn keyword) (assoc :throw-exceptions false))]))
+
 (defn -run
   "Dispatch HTTP requrest and return a HandlerResponse"
   [this ^ParameterValues params]
-  (->
-    (apply
-      (resolve (symbol (str "clj-http.client/" (.toLowerCase (get-value params "method")))))
-      [(get-value params "url")
-       (-> params (get-value "config" "{}") (json/read-str :key-fn keyword) (assoc :throw-exceptions false))])
+  (-> (run
+        (get-value params "method")
+        (get-value params "url")
+        (get-value params "config" "{}"))
     (response-body)))
 
 (defn -name
