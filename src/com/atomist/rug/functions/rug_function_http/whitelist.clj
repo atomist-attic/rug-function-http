9e0c4d0435426b9dd9839380071b38041f716583
James Carnegie <kipz@users.noreply.github.com>
2017-05-24 11:34:34 +0100

    Add atomist webhooks to whitelist

--- src/com/atomist/rug/functions/rug_function_http/whitelist.clj
+++ src/com/atomist/rug/functions/rug_function_http/whitelist.clj
@@ -6,17 +6,18 @@
 
 (def whitelist
   {
-   :github         {:patterns #{#"^https://api\.github\.com/.*$"}}
-   :google         {:patterns #{#"^https://www\.googleapis\.com/.*$"}}
-   :heroku         {:patterns #{#"^https://api\.heroku\.com/.*$"}}
-   :stack-overflow {:patterns #{#"^http://api\.stackexchange\.com/.*$"}}
-   :yahoo          {:patterns #{#"^http://query\.yahooapis\.com/.*$"}}
-   :lebowski       {:patterns #{#"^http://lebowski\.me/.*$"}}
-   :xkcd           {:patterns #{#"^http://xkcd\.com/.*$"}}
-   :npm            {:patterns #{#"^https://registry\.npmjs\.org/.*$"}}
-   :jfrog          {:patterns #{#"^https://.+?\.jfrog\.io/.*$"}}
-   :aws            {:patterns #{#"^https://.+?\.amazonaws\.com/.*$"}}
-   :slack          {:patterns #{#"^https://slack\.com/api/.*$"}}})
+   :github           {:patterns #{#"^https://api\.github\.com/.*$"}}
+   :google           {:patterns #{#"^https://www\.googleapis\.com/.*$"}}
+   :heroku           {:patterns #{#"^https://api\.heroku\.com/.*$"}}
+   :stack-overflow   {:patterns #{#"^http://api\.stackexchange\.com/.*$"}}
+   :yahoo            {:patterns #{#"^http://query\.yahooapis\.com/.*$"}}
+   :lebowski         {:patterns #{#"^http://lebowski\.me/.*$"}}
+   :xkcd             {:patterns #{#"^http://xkcd\.com/.*$"}}
+   :npm              {:patterns #{#"^https://registry\.npmjs\.org/.*$"}}
+   :jfrog            {:patterns #{#"^https://.+?\.jfrog\.io/.*$"}}
+   :aws              {:patterns #{#"^https://.+?\.amazonaws\.com/.*$"}}
+   :slack            {:patterns #{#"^https://slack\.com/api/.*$"}}
+   :atomist-webhooks {:patterns #{#"^https://webhook\.atomist\.com/.*$"}}})
 
 (defn allowed-patterns
   "Return a list of all patterns"
